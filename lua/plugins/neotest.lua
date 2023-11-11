70f4c2855e31105ee8933c91a6f0f57ac76f60e7
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2023-11-11 09:49:22 +0100

    Initial commit

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-python"),
+      },
+    })
+  end,
+}
