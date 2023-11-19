604e57e4d55bb06f736c56668b6f8714755ca5b8
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2023-11-19 23:59:53 +0100

    Add new plugins

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,30 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    "BufReadPre "
+      .. vim.fn.expand("~")
+      .. "/second_brain/**.md",
+    "BufNewFile ~/second_brain/**.md",
+  },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "second_brain",
+        path = "~/second_brain",
+      },
+    },
+
+    -- see below for full list of options 👇
+    lazy = false,
+  },
+}
