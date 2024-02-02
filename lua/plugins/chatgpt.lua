d9c68b472edb2381ea69fe2ef33fabd6057ecbff
Łukasz Zieniewicz <lukzieniewicz@gmail.com>
2024-02-02 08:58:02 +0100

    Add llm plugins

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,18 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup()
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  defaults = {
+    openai_params = {
+      model = "gpt4-turbo-preview",
+    },
+  },
+}
